aa17528a785570c2003ce0417e025e4ab029abb4
Piotr Szmielew <p.szmielew@beinformed.pl>
2012-03-28 20:04:25 +0200

    wykrywanie atrybutu z listy

--- spec/elibri_xml_versions_spec.rb
+++ spec/elibri_xml_versions_spec.rb
@@ -2,18 +2,25 @@ require 'spec_helper'
 
 describe Elibri::XmlVersions do
   
-  it "should return true for same elibri objects" do
+  it "should return true for same basic elibri objects" do
     generated_product = onix_from_mock(:basic_product)
     generated_product_2 = onix_from_mock(:basic_product)
     @elibri_xml_versions = Elibri::XmlVersions.new(generated_product.products.first, generated_product_2.products.first)
     @elibri_xml_versions.diff.should eq([])
   end
   
-  it "should return false for different elibri objects" do
+  it "should return false for different basic elibri objects" do
     generated_product = onix_from_mock(:basic_product, :record_reference => 'fdb8fa072be774d97a97')
     generated_product_2 = onix_from_mock(:basic_product, :record_reference => 'fdb8fa072be774d97a95')
     @elibri_xml_versions = Elibri::XmlVersions.new(generated_product.products.first, generated_product_2.products.first)
     @elibri_xml_versions.diff.should eq([:record_reference])
+  end
+  
+  it "should return true for same book elibri objects" do
+    generated_product = onix_from_mock(:book_example)
+    generated_product_2 = onix_from_mock(:book_example)
+    @elibri_xml_versions = Elibri::XmlVersions.new(generated_product.products.first, generated_product_2.products.first)
+    @elibri_xml_versions.diff.should eq([])
   end
   
 end
