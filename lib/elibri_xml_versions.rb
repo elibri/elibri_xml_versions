c47cb7b1c59a96bcf3659a2c7c8a25004b1d0845
Piotr Szmielew <p.szmielew@beinformed.pl>
2012-03-28 19:55:53 +0200

    wykrywanie atrybutu z listy

--- lib/elibri_xml_versions.rb
+++ lib/elibri_xml_versions.rb
@@ -3,9 +3,22 @@ require 'elibri_api_client'
 require 'elibri_onix_dict'
 
 
+
+
 module Elibri
   
   class XmlVersions
+    
+    #TODO: wyciagnac do innego pliku
+    ATTRIBS = [:elibri_dialect, :height, :width, :thickness, :weight, :ean, :isbn13, :number_of_pages, :duration, 
+                        :file_size, :publisher_name, :publisher_id, :imprint_name, :current_state, :reading_age_from, :reading_age_to, 
+                        :table_of_contents, :description, :reviews, :excerpts, :series, :title, :subtitle, :collection_title,
+                        :collection_part, :full_title, :original_title, :trade_title, :parsed_publishing_date, 
+                        #specific for RELEASE 3_0
+                        :cover_type, :cover_price, :vat, :pkwiu,
+                        :record_reference, :notification_type, :deletion_text,
+                        
+                        ]
     
     attr_accessor :a, :b
     
@@ -14,8 +27,12 @@ module Elibri
       @b = b
     end
     
-    def compare
-      @a == @b
+    def diff
+      diffs = []
+      ATTRIBS.each do |attrib|
+        diffs << attrib if @a.send(attrib) != @b.send(attrib)
+      end
+      diffs
     end
     
   end
--- spec/elibri_xml_versions_spec.rb
+++ spec/elibri_xml_versions_spec.rb
@@ -5,15 +5,15 @@ describe Elibri::XmlVersions do
   it "should return true for same elibri objects" do
     generated_product = onix_from_mock(:basic_product)
     generated_product_2 = onix_from_mock(:basic_product)
-    @elibri_xml_versions = Elibri::XmlVersions.new(generated_product, generated_product_2)
-    @elibri_xml_versions.compare.should eq(true)
+    @elibri_xml_versions = Elibri::XmlVersions.new(generated_product.products.first, generated_product_2.products.first)
+    @elibri_xml_versions.diff.should eq([])
   end
   
   it "should return false for different elibri objects" do
     generated_product = onix_from_mock(:basic_product, :record_reference => 'fdb8fa072be774d97a97')
     generated_product_2 = onix_from_mock(:basic_product, :record_reference => 'fdb8fa072be774d97a95')
-    @elibri_xml_versions = Elibri::XmlVersions.new(generated_product, generated_product_2)
-    @elibri_xml_versions.compare.should eq(false)
+    @elibri_xml_versions = Elibri::XmlVersions.new(generated_product.products.first, generated_product_2.products.first)
+    @elibri_xml_versions.diff.should eq([:record_reference])
   end
   
 end
