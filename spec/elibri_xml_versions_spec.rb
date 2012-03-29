13cd5bd81e16223cc0e122a5765dee86d1bdd2d8
Piotr Szmielew <p.szmielew@beinformed.pl>
2012-03-29 13:58:54 +0200

    added failing spec

--- spec/elibri_xml_versions_spec.rb
+++ spec/elibri_xml_versions_spec.rb
@@ -15,7 +15,7 @@ describe Elibri::XmlVersions do
     @elibri_xml_versions.diff.should eq({:deleted => [], :added => [], :changes => []})
   end
     
-  it "should return changes for different basic elibri objects" do
+  it "should return change for different basic elibri objects" do
     generated_product = onix_from_mock(:basic_product, :record_reference => 'fdb8fa072be774d97a97')
     generated_product_2 = onix_from_mock(:basic_product, :record_reference => 'fdb8fa072be774d97a95')
     @elibri_xml_versions = Elibri::XmlVersions.new(generated_product.products.first, generated_product_2.products.first)
@@ -33,6 +33,13 @@ describe Elibri::XmlVersions do
     generated_product_2 = onix_from_mock(:book_example)
     @elibri_xml_versions = Elibri::XmlVersions.new(generated_product.products.first, generated_product_2.products.first)
     @elibri_xml_versions.diff.should eq({:deleted => [], :added => [], :changes => []})
+  end
+  
+  it "should return change for different book elibri objects" do
+    generated_product = onix_from_mock(:book_example, :isbn_value => '9788324799992')
+    generated_product_2 = onix_from_mock(:book_example, :isbn_value => '9788324799991')
+    @elibri_xml_versions = Elibri::XmlVersions.new(generated_product.products.first, generated_product_2.products.first)
+    @elibri_xml_versions.diff.should eq({:deleted => [], :added => [], :changes => [:isbn_value]})
   end
   
 end
